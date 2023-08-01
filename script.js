e8bb7aa0aeac6483fa68ed8b6cd03dcef4dbe13b
khalidBouh <khaalidm26@gmail.com>
2023-08-01 14:53:51 +0300

    Answered

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello");
+function calculateTip(bill) {
+    let tip;
+    if (bill >= 50 && bill <= 300) {
+      tip = bill * 0.15;
+    } else {
+      tip = bill * 0.2;
+    }
+    return tip;
+  }
+
+const bill = 289;
+const tip = calculateTip(bill);
+console.log(`For bill of ${bill}, the tip is ${tip}`);
